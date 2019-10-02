fed18602aa3c7e9632fdafb86d20de312ecb73b4
Anton Ferkalek <ferkalekanton@gmail.com>
2019-10-02 16:27:59 +0200

    changed ts file

--- src/app/apartment/services/apartment.service.ts
+++ src/app/apartment/services/apartment.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+import { of } from 'rxjs';
+
+import { apartments } from '../models/apartment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApartmentService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  getApartments() {
+    return of(apartments);
+  }
 }
